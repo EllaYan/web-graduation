45986ce1dfa7a12b3dea3eab5659c669e73acf59
Ella <yanliwan>
2019-04-03 23:09:02 +0800

    no message

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,6 +1,6 @@
 <template>
     <el-aside class="aside">
-        <el-menu class="menu-container">
+        <el-menu class="menu-container" active-text-color="#001529">
             <el-menu-item v-for="item in memu" :key="item.name">
                 <i class="el-icon-menu"></i>
                 <span slot="title">{{item.name}}</span>
@@ -24,17 +24,33 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .aside{
     display: flex;
     flex-direction: column;
     min-height: 100vh;
-    background-color: #40C6FF;
+    background-color: #001529;
     color: #fff;
+    width:200px !important;
+    overflow: hidden;
 }
 .menu-container{
-    background-color: #40C6FF;
+    background-color: #001529;
     color:#fff;
+    width:200px;
+    border:0;
+    .el-menu-item{
+        color: #fff;
+        background-color: #001529;
+        &:hover{
+            background-color: #fff !important;
+            color: #001529 !important;
+        }
+    }
+    .is-active{
+        color: #001529 !important;
+    }
+    
 }
 </style>
 
